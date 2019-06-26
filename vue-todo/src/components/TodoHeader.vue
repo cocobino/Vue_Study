97777c16d32b17801cd3738add0e84aa20e5c48e
cocobino <coolbino@naver.com>
2019-06-26 11:53:00 +0900

    vue - project vueHeader , App.vue -> css 설정하기

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        header
-    </div>
+  <header>
+      <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
+// component 안에서만 유요한 스타일 속성 scoped
+<style scoped>
+/* rem : font 의 단위에따라 변화함 */
+    h1{
+        color :#2f3b52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
