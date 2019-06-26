38ffb2ffcf1e519ffb1e239fd91000c2e08a0a6c
cocobino <coolbino@naver.com>
2019-06-26 11:04:08 +0900

    vue - todo app start

---
+++ Novice/vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header></app-header>
+    
+    <app-header v-bind="propsdata"="str"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+
+export default {
+  data : function(){
+    return {
+      str : 'hi'
+    }
+  },
+  components: {
+    'app-header' : AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
--- Novice/vue-cli/src/main.js
+++ Novice/vue-cli/src/main.js
@@ -3,11 +3,7 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
-  var App = {
-    template : '<div>app</div>'
-  }
-
-
+ 
 new Vue({
   render: h => h(App), // Component 내용을 다른파일에서 불러옴 -> 랜더함
   components : {
