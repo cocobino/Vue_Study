06d6a75376f37f951e18d51adbe1a1283c4ce65c
cocobino <coolbino@naver.com>
2019-07-03 11:55:02 +0900

    vue-model 개발 환경섲렁

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,14 +5,27 @@
         <span class = 'addContainer' v-on:click='addTodo'>
             <i class="fas fa-plus addBtn"></i>
         </span>
+
+ <Modal v-if="showModal" @close="showModal = false">
+    <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+    <h3 slot="header">custom header</h3>
+  </Modal>
+
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
+
 export default {
 data: function(){
     return{
-        newTodoItem:""
+        newTodoItem:"",
+        showModal: false,
     }
 },
 methods:{
@@ -26,6 +39,9 @@ methods:{
         this.newTodoItem = '';
         
     }
+},
+components:{
+    Modal : Modal,
 }
 }
 </script>
